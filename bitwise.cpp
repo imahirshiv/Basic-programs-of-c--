91e4c607e0d830c65802c2e8b74f58e0d18702c7
Ahir Shiv <imahirshiv@gmail.com>
2022-11-13 11:54:50 +0530

    initial commit

--- bitwise.cpp
+++ bitwise.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int num1 = 11;
-    cout<<(num1<<2)<<endl;
+    int num1=18;   //1100
+    cout<<(num1<<1)<<endl; //24
+    cout<<(num1>>1)<<endl;  //6
+
+    int num2=13;    //1000
+    cout<<(num1&num2)<<endl;    //1000
+    cout<<(num1|num2)<<endl;    //12
+    cout<<sizeof(num1)<<endl;
+    cout<<(&num2)<<endl;
+
+    int a=5;
+    int b=6;
+    cout<<(a++)<<endl;
+    cout<<(++a)<<endl; 
+
+
     return 0;
 }
