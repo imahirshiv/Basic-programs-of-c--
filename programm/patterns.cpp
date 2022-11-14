1f044269649f7032539f05b0148016b444556703
Ahir Shiv <imahirshiv@gmail.com>
2022-11-14 13:42:44 +0530

    intial commit

---
+++ programm/patterns.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+    cout << "Rows:-";
+    cin >> n;
+    cout << "colomn:-";
+    cin >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (i == 1 || j == 1 || i == n || j == m)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
