f8d6dac64fe4416834192f5d466456617d2b32da
Ahir Shiv <imahirshiv@gmail.com>
2022-11-16 09:46:43 +0530

    function

--- functions/add.cpp
+++ functions/add.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 using namespace std;
-int add(int num1,int num2){
-        int sum=num1+num2;
-        return sum;
+int add(int num1, int num2)
+{
+    int sum = num1 + num2;
+    return sum;
 }
-int main(){
-    int a=5;
-    int b=6;
-    cout<<add(a,b)<<endl;
+int add(int num1, int num2, int num3)
+{
+    int sum = num1 + num2 + num3;
+    return sum;
+}
+int main()
+{
+    int a = 5;
+    int b = 6;
+    int c = 4;
+    cout << add(a, b,4) << endl;
+    // cout << add(a, b, c) << endl;
+    return 0;
 }
