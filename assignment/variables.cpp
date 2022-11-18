7671371d27fb42476214c607d1576158b25a04c1
Ahir Shiv <imahirshiv@gmail.com>
2022-11-18 09:24:44 +0530

    intial commit

---
+++ assignment/variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int x,y;
+    cout<<"x:-";
+    cin>>x;
+    cout<<"y:-";
+    cin>>y;
+int product=x*y;
+cout<<product<<endl;
+    
+
+
+
+
+
+    return 0;
+}
