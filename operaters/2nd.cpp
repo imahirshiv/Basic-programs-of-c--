d312c2d48d06e540e6c9d44304a5e4ddd0849107
Ahir Shiv <imahirshiv@gmail.com>
2022-11-18 11:36:16 +0530

    intial commit

--- operaters/2nd.cpp
+++ operaters/2nd.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
+    int a, b;
+    cout<<"Enter the value of a:-";
+    cin >> a;
+    cout<<"Enter the value of b:-";
+    cin >>b;
 
-    
-
-
+    cout<<((a < 50) && (a < b))?1:0;
+    cout<<endl;
 
     return 0;
 }
