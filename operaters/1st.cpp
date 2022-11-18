986e46ae72947eb67101a5b4cb3b1c08c83ea21c
Ahir Shiv <imahirshiv@gmail.com>
2022-11-18 11:19:33 +0530

    intial commit

---
+++ operaters/1st.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int num1, num2;
+    cin >> num1;
+    cin >> num2;
+
+    if (num1 != num2)
+    {
+        cout << false << endl;
+    }
+    else
+    {
+        cout << true << endl;
+    }
+
+    return 0;
+}
