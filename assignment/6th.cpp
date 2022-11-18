9d8a4c5e86291c4e73be57c586dca63bcf5bc8fb
Ahir Shiv <imahirshiv@gmail.com>
2022-11-18 11:11:26 +0530

    intial commit

--- assignment/6th.cpp
+++ assignment/6th.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main()
 {
 
-    int a, b,temp;
+    int a, b, temp;
     cout << "Enter A:-";
     cin >> a;
     cout << "Enter B:-";
     cin >> b;
-    temp =a;
-    a=b;
-    b=temp;
-    cout<<"After swaping A is:-" << a << endl;
-    cout<<"After swaping B is:-" << b << endl;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "After swaping A is:-" << a << endl;
+    cout << "After swaping B is:-" << b << endl;
 
     return 0;
 }
