1b1fa4004f1d647c226e53def67639a014da1cc7
Ahir Shiv <imahirshiv@gmail.com>
2022-11-18 12:10:23 +0530

    intial commit

---
+++ operaters/3rd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int girls, boys, total;
+    boys = 17;
+    total = (80 * 45) / 100;
+    girls = total - boys;
+    cout << "Total girls getting grade 'A' is :-" << girls << endl;
+
+    return 0;
+}
