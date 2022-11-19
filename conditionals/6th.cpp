b2676282d54583064a6b6ff36f58023ddcf7ee05
Ahir Shiv <imahirshiv@gmail.com>
2022-11-19 14:49:18 +0530

    inital commit

---
+++ conditionals/6th.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int marks;
+    cout << "Enter your marks:";
+    cin >> marks;
+
+    if (marks >= 90)
+    {
+        cout << "Your grade is A+" << endl;
+    }
+    else if (marks >= 80)
+    {
+        cout << "Your grade is A" << endl;
+    }
+    else if (marks >= 70)
+    {
+        cout << "Your grade is B+" << endl;
+    }
+    else if (marks >= 60)
+    {
+        cout << "Your grade is B" << endl;
+    }
+    else if (marks >= 50)
+    {
+        cout << "Your grade is C" << endl;
+    }
+    else if (marks >= 40)
+    {
+        cout << "Your grade is D" << endl;
+    }
+    else if (marks >= 30)
+    {
+        cout << "Your grade is E" << endl;
+    }
+    else
+    {
+        cout << "Your grade is F"<<endl;
+    }
+
+    return 0;
+}
