8f66a43fa0d073a7e55f338fca26f020231255ed
Ahir Shiv <imahirshiv@gmail.com>
2022-11-20 15:38:42 +0530

    inital commit

---
+++ vector/2nd.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector <int> v(5);
+    
+    cout<<"Size "<<v.size()<<endl;
+    cout<<"Capacity"<<v.capacity()<<endl;
+
+    return 0;
+}
