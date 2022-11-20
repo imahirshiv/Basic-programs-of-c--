5437007c88c6e87480d92b7977b26c529ed54133
Ahir Shiv <imahirshiv@gmail.com>
2022-11-20 22:29:49 +0530

    intial commit

---
+++ functions/4th.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+bool prime(int num){
+
+        for (int i = 2; i < num; i++)
+        {
+            if (num%i==0)
+            {
+                return false;
+            }
+            
+        }
+    return true;
+}
+int main(){
+
+        int a;
+        int b;
+        cout<<"Enter two number :";
+        cin>>a>>b;
+        for (int i = a; i <= b; i++)
+        {
+            if (prime(i))
+            {
+                cout<<i<<" ";
+            }
+            
+        }
+        
+
+
+    return 0;
+}
