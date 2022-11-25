19e995f43cbe13828986c969b48a6983b82d584d
Ahir Shiv <imahirshiv@gmail.com>
2022-11-25 11:11:27 +0530

    intial commit

---
+++ Revision/1st.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // integer data type
+    int apple = 5;
+    cout << apple << endl;
+    
+    cout << sizeof(apple) << endl;
+    
+    // character data type
+
+    char sign = '$';
+   
+    cout << sizeof(sign) << endl;
+ 
+    //boolen datatype
+    bool flag = true;
+    cout << flag << endl;
+    float numb = 4.354;
+    cout << sizeof(numb) << endl;
+
+    return 0;
+}
