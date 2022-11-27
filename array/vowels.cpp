470d1e19f538cdc5b9aed4420b4712e521a59873
Ahir Shiv <imahirshiv@gmail.com>
2022-11-27 17:19:31 +0530

    initial commit

--- array/vowels.cpp
+++ array/vowels.cpp
@@ -3,18 +3,27 @@ using namespace std;
 int main(){
 
         char vowels[5];
-         cout<<"Enter vowels you want to print:";
+         cout<<"Enter vowels you want to print:"<<endl;
 
-        for (int idx = 0; idx < 5; idx++)
-        {
-            cin>>vowels[idx];
+        // for (int idx = 0; idx < 5; idx++)
+        // {
+        //     cin>>vowels[idx];
+        // }
+        // for (int ix = 0; ix <5; ix++)
+        // {
+        //     cout<<vowels[ix]<<" ";
+        // }
+        //for each loop input 
+
+        for(char &element:vowels){
+            cin>>element;
+
         }
         for (int idx = 0; idx <5; idx++)
         {
             cout<<vowels[idx]<<" ";
         }
+        cout<<"\n";
         
-        
-
     return 0;
 }
