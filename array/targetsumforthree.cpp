12ef7a0068ab07c5b0b43f943d08889c88925ec8
Ahir Shiv <imahirshiv@gmail.com>
2022-11-28 15:52:18 +0530

    intial commit

--- array/targetsumforthree.cpp
+++ array/targetsumforthree.cpp
@@ -1,28 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 
-        int array[]={3,1,2,4,0,6};
-        int targatesum=6;
-        int size=6;
-        int triplets=0;
-        for (int i = 0; i < size; i++)
+    int array[] = {3, 1, 2, 4, 0, 6};
+    int targatesum = 6;
+    int size = 6;
+    int triplets = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
         {
-            for (int j = i+1; j < size; j++)
+            for (int k = j + 1; k < size; k++)
             {
-                for (int k = j+1; k < size; k++)
+                if (array[i] + array[j] + array[k] == targatesum)
                 {
-                    if (array[i]+array[j]+array[k]==targatesum)
-                    {
-                        triplets++;
-                    }
-                    
+                    triplets++;
                 }
-                
             }
-            
         }
-        cout<<triplets<<endl;
+    }
+    cout << triplets << endl;
 
     return 0;
 }
