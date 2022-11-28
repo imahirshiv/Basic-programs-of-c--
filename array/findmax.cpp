aaf53c3c265cf25ebe40b24399e43b4dacfa22e0
Ahir Shiv <imahirshiv@gmail.com>
2022-11-28 09:12:30 +0530

    intial commit

--- array/findmax.cpp
+++ array/findmax.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main()
 {
     int myarray[]={2,5,6,9,11};
-    int key=9;
+    int key=2;
     int ans=-1;
     for (int i = 0; i < 5; i++)
     {     
-        if (myarray[i]=key)
+        if (myarray[i]==key)
         {
-            cout<<key<<endl;
+            ans=i;
         }
-        
+
     }
-    
+    cout<<ans<<endl;
     return 0;
 }
