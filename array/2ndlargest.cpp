a2f736674ef83525b3aa1ccb2e46cb6b452b8d74
Ahir Shiv <imahirshiv@gmail.com>
2022-11-28 16:36:07 +0530

    initial commit

---
+++ array/2ndlargest.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int largestelementindex(int array[],int size){
+    int max=INT8_MIN;
+    for (int  i = 0; i < size; i++)
+    {
+        if (array[i]>max)
+        {
+            max=array[i];
+        }
+        
+    }
+return max;    
+
+}
+int main(){
+     
+     int array[]={2,3,5,7,6,1};
+    int largestel=largestelement(array,6);
+
+
+    return 0;
+}
