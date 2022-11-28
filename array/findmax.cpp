bd5752f61580d328f92b588ec538b1d87986d0aa
Ahir Shiv <imahirshiv@gmail.com>
2022-11-28 10:20:01 +0530

    intialc ommit

--- array/findmax.cpp
+++ array/findmax.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int myarray[]={2,5,6,9,11};
+    int myarray[]={2,5,8,9,11};
     int key=2;
     int ans=-1;
     for (int i = 0; i < 5; i++)
@@ -10,6 +10,7 @@ int main()
         if (myarray[i]==key)
         {
             ans=i;
+            break;
         }
 
     }
