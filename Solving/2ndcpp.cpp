d56ac8b801bdd90fa396e6528c1ca5977f1ff768
Ahir Shiv <imahirshiv@gmail.com>
2022-11-29 16:41:28 +0530

    inital commit

--- Solving/2ndcpp.cpp
+++ Solving/2ndcpp.cpp
@@ -1,48 +1,46 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-void sortZerosAndOnes(vector<int> &v){
-    int zerosCount=0;
-    for(int ele:v){
-        if (ele==0)
+void sortZerosAndOnes(vector<int> &v)
+{
+    int zerosCount = 0;
+    for (int ele : v)
+    {
+        if (ele == 0)
         {
             zerosCount++;
         }
-        
     }
     for (int i = 0; i < v.size(); i++)
     {
-        if (i<zerosCount)
+        if (i < zerosCount)
         {
-            v[i]=0;
+            v[i] = 0;
         }
         else
         {
-            v[i]=1;
+            v[i] = 1;
         }
-        
-        
     }
-    
 }
-int main(){
+int main()
+{
 
     int n;
-    cin>>n;
+    cin >> n;
     vector<int> v;
     for (int i = 0; i < n; i++)
     {
-        int elm; cin>>elm;
+        int elm;
+        cin >> elm;
         v.push_back(elm);
     }
     sortZerosAndOnes(v);
     for (int i = 0; i < n; i++)
     {
-        cout<<v[i]<<" ";
-
+        cout << v[i] << " ";
     }
-    cout<<endl;
-    
+    cout << endl;
 
     return 0;
 }
---
+++ Solving/3rd.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void sortByParity(vector<int> &v){
+    int left_ptr=0;
+    int right_ptr=v.size()-1;
+while (left_ptr<right_ptr)
+{
+    if (v[left_ptr]%2==1 && v[right_ptr]%2==0)
+    {
+        swap(v[left_ptr],v[right_ptr]);
+        left_ptr++; right_ptr--;
+    }
+    if (v[left_ptr]%2==0)
+    {
+        left_ptr++;
+    }
+    if (v[right_ptr]%2==1)
+    {
+        right_ptr--;
+    }
+    
+    
+    
+}
+return;
+
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int elm;
+        cin>>elm;
+        v.push_back(elm);
+    }
+    sortByParity(v);
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+
+    
+    
+
+    return 0;
+}
