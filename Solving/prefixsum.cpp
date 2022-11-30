05b7be10f76a88cbce1f8dca53844d4431b7e4a9
Ahir Shiv <imahirshiv@gmail.com>
2022-11-30 10:00:03 +0530

    v

--- Solving/prefixsum.cpp
+++ Solving/prefixsum.cpp
@@ -1,35 +1,35 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-void runningSum(vector<int> &v){
+void runningSum(vector<int> &v)
+{
     for (int i = 1; i < v.size(); i++)
     {
-        v[i]=v[i-1]+v[i];
+        v[i] = v[i - 1] + v[i];
     }
-    return ;
+    return;
 }
 
-int main(){
+int main()
+{
 
     int n;
-    cin>>n;
+    cin >> n;
 
     vector<int> v;
     for (int i = 0; i < n; i++)
     {
         int ele;
-        cin>>ele;
+        cin >> ele;
         v.push_back(ele);
     }
     runningSum(v);
 
     for (int i = 0; i < n; i++)
     {
-        cout<<v[i]<<" ";
-
+        cout << v[i] << " ";
     }
-    cout<<endl;
-    
+    cout << endl;
 
     return 0;
 }
