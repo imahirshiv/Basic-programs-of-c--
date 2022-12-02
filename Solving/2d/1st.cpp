abc8ca3d9573f43b224be53369383716dc621540
Ahir Shiv <imahirshiv@gmail.com>
2022-12-02 21:27:56 +0530

    inital commit

--- Solving/2d/1st.cpp
+++ Solving/2d/1st.cpp
@@ -1,25 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 
-    int n,m;
-    cin>>n>>m;
+    int n, m;
+    cin >> n >> m;
     int array[n][m];
+
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; i < m; j++)
+        for (int j = 0; j < m; j++)
         {
-            cin>>array[i][j];
+            cin >> array[i][j];
         }
-        
+    }
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; i < m; j++)
+        for (int j = 0; j < m; j++)
         {
-            cout<<array[i][j]<<" ";
-        }cout<<endl;
-        
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
     }
-    }
+
     return 0;
 }
--- Solving/2d/2nd.cpp
+++ Solving/2d/2nd.cpp
@@ -1,11 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 
-    int n;
-    cin>n;
+    int n, m;
+    cin >> n >> m;
+    int array[n][m];
 
-    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> array[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
