6b54fd970c64193daeca1e94cb1351269ed47650
Ahir Shiv <imahirshiv@gmail.com>
2022-12-10 08:28:51 +0530

    intialoc

--- Solving/2d/3rdrd.cpp
+++ Solving/2d/3rdrd.cpp
@@ -1,6 +1,51 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+int maximumOnesRow(vector<vector<int>> &V){
+
+    int maxOnes=INT16_MIN;
+    int maxOnesRow=-1;
+    int columns=V[0].size();
+
+    for (int i = 0; i < V.size(); i++)
+    {
+        for (int j = 0; j <V[i].size(); j++)
+        {
+            if (V[i][j]==1)
+            {
+                int numberOfOnes=columns-j;
+                if (numberOfOnes>maxOnes)
+                {
+                    maxOnes=numberOfOnes;
+                    maxOnesRow=i;
+                }
+                
+            }
+            
+        }
+        
+    }
+    return maxOnesRow;
+
+}
+
 int main(){
+    int n,m;
+    cin>>n>>m;
+
+    vector<vector<int>> vec(n,vector<int>(m));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin>>vec[i][j];
+        }
+        
+    }
     
+    int res = maximumOnesRow(vec);
+    cout<<res<<endl;
+
     return 0;
 }
